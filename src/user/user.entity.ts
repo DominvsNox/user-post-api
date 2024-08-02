f9638b5eab1eb0fed36a52416ccd54a4bdac8aaa
Yaroslav Lepetchenko <yaroslav.lepetchenko@discoverember.com>
2024-08-02 12:24:47 +0300

    ADD: swagger documentation

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,18 +1,22 @@
 import { Role } from "src/enums/role.enum";
 import { Post } from "src/post/post.entity";
 import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({name: 'user'})
 export class User {
     @PrimaryGeneratedColumn({name: 'id'})
     id: number;
 
+    @ApiProperty()
     @Column({name: 'name'})
     name: string;
 
+    @ApiProperty()
     @Column({name: 'password'})
     password: string;
 
+    @ApiProperty()
     @Column({name: 'role', default: 'user'})
     role: Role;
 
